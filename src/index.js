9de1e6a5f96ff3500466f490f3ced1fa8a0fa845
laureenakinyi <laureen.akinyi@student.moringaschool.com>
2022-06-20 07:36:36 +0300

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const init = () => {
-  
-}
+const submitData = () => {
+    const form = document.querySelector('form');
 
-document.addEventListener('DOMContentLoaded', init);
+    const inputData = document.getElementById('searchByID');
+    form.addEventListener ('submit', (event) => {
+        event.preventDefault();
+
+    fetch('http://localhost:3000/movies/${inputDAta.value}')
+        .then (response => response.json())
+        .then (results => {const Title = document.querySelector('h4')
+        const Summary = document.querySelector ('p')
+   Title.innerText = results.title
+Summary.innerText = results.summary})
+    });
+} 
+
+document.addEventListener('DOMContentLoaded', submitData);
+
